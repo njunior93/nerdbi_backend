2a0e041db3e073714f5380415528383a16e06a47
Natanael Junior <naeljunior07@gmail.com>
2026-06-16 00:11:56 -0300

    teste3

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
+  origin:[
       process.env.FRONTEND_URL,
       'http://localhost:5173',
   ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  methods:'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders:['Content-Type', 'Authorization'],
+  credentials:true,
   })
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
