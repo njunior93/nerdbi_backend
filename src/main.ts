85fe51a373311c6d4bafcdd5bf1a9d3be284daea
Natanael Junior <naeljunior07@gmail.com>
2026-06-16 00:00:57 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
-      process.env.FRONTEND_URL,
-      'http://localhost:5173',
-  ],
+  origin: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
