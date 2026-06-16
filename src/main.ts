b97fab3c27f0e9ce7b50e61d54bcba7f10c6a9ca
Natanael Junior <naeljunior07@gmail.com>
2026-06-15 23:36:22 -0300

    refact:configuração do arquivo main.ts para o 'CORS'

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: ['http://localhost:5173'],
+  origin: [
+      process.env.JWT_TOKEN_ISSUER,
+      process.env.JWT_TOKEN_AUDIENCE
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
